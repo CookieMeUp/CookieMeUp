03f0176ccfcaf2908221f77d9ce124b65ad862d8
Daniel Calderon <dcalderonmariavez@csumb.edu>
2018-04-02 14:34:54 -0700

    Fixing git ignore

---
+++ CookieMeUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CookieMeUp
+//
+//  Created by Daniel Calderon on 2/26/18.
+//  Copyright © 2018 Daniel Calderon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
