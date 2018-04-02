70ea8913ce782cf5fd9433c9ba062866d8980b16
Daniel Calderon <dcalderonmariavez@csumb.edu>
2018-04-02 15:33:16 -0700

    Adding custon COlors

---
+++ CookieMeUp/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  CookieMeUp
+//
+//  Created by Daniel Calderon on 4/2/18.
+//  Copyright © 2018 Daniel Calderon. All rights reserved.
+//
+
+import Foundation
+
+let black = "#000000"
+let red_pink = "#FF465C"
+let light_blue = "#55FCEA"
+let green = "#00DE8F"
+let white = "#FBFFFF"
